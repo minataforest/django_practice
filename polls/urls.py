e63d5c5188f6c5a2f9432e61aff4332b754a0211
minataforest <minataforest@gmail.com>
2022-03-07 22:00:12 +0900

    220307 1 17/25

--- polls/urls.py
+++ polls/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/result/', views.result, name='result'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
