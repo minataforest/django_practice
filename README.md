ccc62d131ccead6c6fc06be6085a2f75ae451945
minataforest <minataforest@gmail.com>
2022-03-08 22:32:53 +0900

    220308 1 25/25 complete

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 django 연습   
 
 1. [https://www.inflearn.com/course/나의-첫-django-앱-만들기/](https://www.inflearn.com/course/%EB%82%98%EC%9D%98-%EC%B2%AB-django-%EC%95%B1-%EB%A7%8C%EB%93%A4%EA%B8%B0/)  
-from 22.03.03 ~ 
+from 22.03.03 ~ 22.03.08
--- polls/views.py
+++ polls/views.py
@@ -1,26 +1,64 @@
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+
+from .models import Question, Choice
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list
-    }
-    return render(request, 'polls/index.html', context)
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list
+#     }
+#     return render(request, 'polls/index.html', context)
 
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        # Return the last five published questions.
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def result(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+#
+# def result(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/result.html', {'question': question})
+
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/result.html'
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # 에러 메세지와 함께 폼을 다시 디스플레이합니다.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # POST 데이터 처리를 정상적으로 마친 뒤에는 항상 HttpResponseRedirect를 리턴합니다.
+        # 이 방법을 통해 유저가 브라우저의 "뒤로가기"을 눌렀을 때
+        # 데이터가 두 번 저장되는 것을 방지할 수 있습니다.
+        return HttpResponseRedirect(reverse('polls:result', args=(question.id,)))
