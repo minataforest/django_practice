582380fe8fe59104544e9a441868e3d90b5a95ce
minataforest <minataforest@gmail.com>
2022-03-08 23:18:17 +0900

    220308 black formmater git hook test

--- polls/views.py
+++ polls/views.py
@@ -6,7 +6,6 @@ from .models import Question, Choice
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
 
-
 # def index(request):
 #     latest_question_list = Question.objects.order_by('-pub_date')[:5]
 #     template = loader.get_template('polls/index.html')
@@ -17,12 +16,12 @@ from django.shortcuts import render, get_object_or_404
 
 
 class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
     def get_queryset(self):
         # Return the last five published questions.
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.order_by("-pub_date")[:5]
 
 
 # def detail(request, question_id):
@@ -32,7 +31,8 @@ class IndexView(generic.ListView):
 
 class DetailView(generic.DetailView):
     model = Question
-    template_name = 'polls/detail.html'
+    template_name = "polls/detail.html"
+
 
 #
 # def result(request, question_id):
@@ -42,23 +42,63 @@ class DetailView(generic.DetailView):
 
 class ResultView(generic.DetailView):
     model = Question
-    template_name = 'polls/result.html'
+    template_name = "polls/result.html"
 
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
         # 에러 메세지와 함께 폼을 다시 디스플레이합니다.
-        return render(request, 'polls/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
     else:
         selected_choice.votes += 1
         selected_choice.save()
         # POST 데이터 처리를 정상적으로 마친 뒤에는 항상 HttpResponseRedirect를 리턴합니다.
         # 이 방법을 통해 유저가 브라우저의 "뒤로가기"을 눌렀을 때
         # 데이터가 두 번 저장되는 것을 방지할 수 있습니다.
-        return HttpResponseRedirect(reverse('polls:result', args=(question.id,)))
+        return HttpResponseRedirect(reverse("polls:result", args=(question.id,)))
+
+
+def test_black(request):
+
+    x = {"a": 37, "b": 42, "c": 927}
+
+    x = 123456789.123456789e123456789
+
+    if (
+        very_long_variable_name is not None
+        and very_long_variable_name.field > 0
+        or very_long_variable_name.is_debug
+    ):
+        z = "hello " + "world"
+    else:
+        world = "world"
+        a = "hello {}".format(world)
+        f = rf"hello {world}"
+        if this and that:
+            y = "hello " "world"  # FIXME: https://github.com/python/black/issues/26
+
+    data = 37 - 5
+
+    regular_formatting = [
+        0,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+    ]
+
+    return HttpResponse(1)
