e968d3b509470b6d9cd89a197334479841ef3b4d
minataforest <minataforest@gmail.com>
2022-04-18 22:55:47 +0900

    220418 celery3

--- celery_test/urls.py
+++ celery_test/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from celery_test.views import celery_test
+from celery_test.views import celery_test, celery_test2
 
 app_name = "celery"
 
 urlpatterns = [
     path("celery_test/", celery_test, name="celery_test"),
+    path("celery_test2/", celery_test2, name="celery_test2"),
 ]
--- celery_test/views.py
+++ celery_test/views.py
@@ -6,5 +6,11 @@ from celery_test.tasks import mul, add
 
 
 def celery_test(request):
-    result = mul.delay(2)
+    result = mul.delay(2, 4)
+    return HttpResponse(result.get())
+
+
+def celery_test2(request):
+    s1 = add.s(2, 4)
+    result = s1.delay()
     return HttpResponse(result.get())
